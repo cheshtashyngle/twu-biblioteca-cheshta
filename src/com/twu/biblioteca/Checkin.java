dfbf7a9ab78d8f920c69f26050f09932f56f77cf
Cheshta <cheshu_25@yahoo.co.in>
2015-07-31 16:37:47 +0530

    [Cheshta] [#0012] Application asks library to checkin book according to the user request

--- src/com/twu/biblioteca/Checkin.java
+++ src/com/twu/biblioteca/Checkin.java
@@ -13,6 +13,6 @@ public class Checkin implements Command {
 
     @Override
     public void execute() {
-
+        library.checkin(bookName);
     }
 }
---
+++ test/com/twu/biblioteca/CheckinTest.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class CheckinTest {
+    @Test
+    public void shouldTellLibraryToTellWhatBooksIsHasAndAskViewToPrint() {
+        Library library = mock(Library.class);
+        View view = mock(View.class);
+        Checkin checkin = new Checkin(library, view, "Head First With Java");
+
+        checkin.execute();
+
+        verify(library).checkin("Head First With Java");
+    }
+}
